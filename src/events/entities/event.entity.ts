372019951dbeb0d2df95b4a90130c9b6ad75b681
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2023-11-30 20:26:23 +0530

    Event Entity

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,1 +1,22 @@
-export class Event {}
+import { Prop, Schema } from '@nestjs/mongoose';
+// import mongoose, { ObjectId } from 'mongoose';
+
+@Schema()
+export class Event {
+  @Prop()
+  name: string;
+
+  @Prop() startDate: Date;
+
+  @Prop() endDate: Date;
+
+  @Prop() description: string;
+
+  @Prop() venue: string;
+
+  @Prop({ required: true }) isApproved: boolean;
+
+  //   TODO:Find How to do it, in the context of microservice
+  @Prop({ required: true }) createdBy: string;
+  @Prop({ required: true }) approvedBy: string;
+}
