33735ee94d5669bf8b81d874cc17f4636d921326
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2023-12-01 15:17:20 +0530

    Connect MongoDB db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EventsModule } from './events/events.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [EventsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/alumni-events'),
+    EventsModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      // Explore
       transport: Transport.TCP,
     },
   );
