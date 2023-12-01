6e4ceff2cba50e73035fb24a9d13b410c296440f
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2023-12-01 15:27:55 +0530

    Event DTO

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,1 +1,23 @@
-export class CreateEventDto {}
+import { IsBoolean, IsDate, IsString } from 'class-validator';
+export class CreateEventDto {
+  @IsString()
+  name: string;
+
+  @IsDate()
+  startDate: Date;
+
+  @IsDate()
+  endDate: Date;
+
+  @IsString()
+  venue: string;
+
+  @IsBoolean()
+  isApproved: boolean;
+
+  @IsString()
+  createdBy: string;
+
+  @IsString()
+  approvedBy: string;
+}
